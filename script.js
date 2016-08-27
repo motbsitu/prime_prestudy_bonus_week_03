74093b05744c3f6f117f7f665fbc15e45a99af1b
Julie S Mike <juliesmike@gmail.com>
2016-08-26 19:04:49 -0500

    Change in function in js, update in html and css for layout

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-var number = 99;
-while (number <=100){
-    console.log("Hey, this works!");
-    number++;
+
+function numChallenge(a, b, c, d) {
+    return ((a + b) *c) -d;
 }
+console.log(numChallenge(1,2,3,4));
